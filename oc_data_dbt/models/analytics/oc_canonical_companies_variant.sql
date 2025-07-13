9d49fdf22b1022f3c38ebf252ee651d6ec71ac3b
Rishab2469 <rishabaggarwal412@gmail.com>
2025-07-13 15:20:40 +0530

    Add snowflake config

--- oc_data_dbt/models/analytics/oc_canonical_companies_variant.sql
+++ oc_data_dbt/models/analytics/oc_canonical_companies_variant.sql
@@ -5,7 +5,7 @@
 
 WITH officers AS (
     SELECT
-        dos_id as entity_id,
+        entity_id,
         ARRAY_AGG(
             OBJECT_CONSTRUCT(
                 'name', ceo_name,
@@ -23,14 +23,14 @@ WITH officers AS (
                 )
             )
         ) AS officers
-    FROM {{ ref('stg_us_ny_companies') }}
+    FROM {{ ref('s_us_ny_companies') }}
     WHERE ceo_name IS NOT NULL 
         AND ceo_name NOT IN ('', 'none', 'n/a', '.', ',', '-', 'no information available', 'REGISTERED AGENT REVOKED', 'REGISTERED AGENT RESIGNED')
-    GROUP BY dos_id
+    GROUP BY entity_id
 ),
 agents AS (
     SELECT
-        dos_id as entity_id,
+        entity_id,
         ARRAY_AGG(
             OBJECT_CONSTRUCT(
                 'name', registered_agent_name,
@@ -48,30 +48,34 @@ agents AS (
                 )
             )
         ) AS agents
-    FROM {{ ref('stg_us_ny_companies') }}
+    FROM {{ ref('s_us_ny_companies') }}
     WHERE registered_agent_name IS NOT NULL 
         AND registered_agent_name NOT IN ('', 'none', 'n/a', '.', ',', '-', 'no information available', 'REGISTERED AGENT REVOKED', 'REGISTERED AGENT RESIGNED')
-    GROUP BY dos_id
+    GROUP BY entity_id
 )
 SELECT
-    s.dos_id as entity_id,
+    s.entity_id,
     s.current_entity_name AS name,
     s.entity_type AS company_type,
     s.initial_dos_filing_date as incorporation_date,
     s.jurisdiction AS jurisdiction_code,
     s.county,
-    OBJECT_CONSTRUCT(
-        'care_of', s.dos_process_name,
-        'street_address', 
-            CASE 
-                WHEN s.dos_process_address_1 IS NOT NULL AND s.dos_process_address_2 IS NOT NULL AND s.dos_process_address_2 != ''
-                    THEN s.dos_process_address_1 || ', ' || s.dos_process_address_2
-                ELSE COALESCE(s.dos_process_address_1, s.dos_process_address_2)
-            END,
-        'locality', s.dos_process_city,
-        'region', s.dos_process_state,
-        'postal_code', s.dos_process_zip
-    ) AS registered_address,
+    CASE 
+    WHEN s.dos_process_name IS NOT NULL THEN
+        OBJECT_CONSTRUCT(
+            'care_of', s.dos_process_name,
+            'street_address', 
+                CASE 
+                    WHEN s.dos_process_address_1 IS NOT NULL AND s.dos_process_address_2 IS NOT NULL AND s.dos_process_address_2 != ''
+                        THEN s.dos_process_address_1 || ', ' || s.dos_process_address_2
+                    ELSE COALESCE(s.dos_process_address_1, s.dos_process_address_2)
+                END,
+            'locality', s.dos_process_city,
+            'region', s.dos_process_state,
+            'postal_code', s.dos_process_zip
+        )
+    ELSE NULL
+    END AS registered_address,
     officers.officers,
     agents.agents,
     OBJECT_CONSTRUCT(
@@ -85,14 +89,14 @@ SELECT
     ) AS all_attributes,
     _meta_stg_file_name as _meta_stg_file_name,
     _meta_stg_file_last_modified as _meta_stg_file_last_modified
-FROM {{ ref('stg_us_ny_companies') }} s
-LEFT JOIN officers ON s.dos_id = officers.entity_id
-LEFT JOIN agents ON s.dos_id = agents.entity_id
+FROM {{ ref('s_us_ny_companies') }} s
+LEFT JOIN officers ON s.entity_id = officers.entity_id
+LEFT JOIN agents ON s.entity_id = agents.entity_id
 WHERE
     s.current_entity_name IS NOT NULL
     AND s.current_entity_name NOT IN ('', 'none', 'n/a', '.', ',', '-', 'no information available')
-    AND s.dos_id IS NOT NULL
-    AND s.dos_id NOT IN ('', 'none', 'n/a', '.', ',', '-', 'no information available')
+    AND s.entity_id IS NOT NULL
+    AND s.entity_id NOT IN ('', 'none', 'n/a', '.', ',', '-', 'no information available')
     AND s.initial_dos_filing_date IS NOT NULL
     AND s.entity_type NOT IN (
         'DOMESTIC BUSINESS CORPORATION RESERVATION',
--- oc_data_dbt/models/raw/stg_us_ny_companies_raw.sql
+++ oc_data_dbt/models/raw/stg_us_ny_companies_raw.sql
@@ -1,23 +1,51 @@
 {{ 
     config(
-    materialized='view', 
-    post_hook='{{ create_external_tables() }}'
+        materialized = 'table',
+        post_hook = "
+            COPY INTO {{this}}
+            FROM @my_s3_stage
+            FILE_FORMAT = (
+                TYPE = 'CSV'
+                SKIP_HEADER = 1
+                FIELD_OPTIONALLY_ENCLOSED_BY = '\"'
+            )
+        "
     ) 
 }}
--- This is a raw layer model for NY jurisdiction, data will be loaded from post hook.
+
+
+
+-- This creates an empty table (but COPY will fill it)
 SELECT
-    NULL AS _meta_load_id,
-    NULL AS _meta_load_timestamp,
-    NULL AS _meta_load_name,
-    NULL AS _meta_load_version,
-    NULL AS _meta_stg_file_name,
-    NULL AS _meta_stg_file_last_modified,
-    NULL AS _meta_stg_file_row_number,
-    NULL AS _meta_stg_file_hash,
-    NULL AS _meta_source_system,
-    NULL AS _meta_source_entity,
-    NULL AS _meta_country,
-    NULL AS _meta_jurisdiction,
-    NULL AS _meta_registration_authority_code,
-    NULL AS FULL_ROW_CSV
-WHERE FALSE
+    -- Business columns
+    CAST(NULL AS VARCHAR(100)) AS dos_id,
+    CAST(NULL AS VARCHAR(100)) AS current_entity_name,
+    CAST(NULL AS VARCHAR(100)) AS initial_dos_filing_date,
+    CAST(NULL AS VARCHAR(100)) AS county,
+    CAST(NULL AS VARCHAR(100)) AS jurisdiction,
+    CAST(NULL AS VARCHAR(100)) AS entity_type,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_name,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_address_1,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_address_2,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_city,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_state,
+    CAST(NULL AS VARCHAR(100)) AS dos_process_zip,
+    CAST(NULL AS VARCHAR(100)) AS ceo_name,
+    CAST(NULL AS VARCHAR(100)) AS ceo_address_1,
+    CAST(NULL AS VARCHAR(100)) AS ceo_address_2,
+    CAST(NULL AS VARCHAR(100)) AS ceo_city,
+    CAST(NULL AS VARCHAR(100)) AS ceo_state,
+    CAST(NULL AS VARCHAR(100)) AS ceo_zip,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_name,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_address_1,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_address_2,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_city,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_state,
+    CAST(NULL AS VARCHAR(100)) AS registered_agent_zip,
+    CAST(NULL AS VARCHAR(100)) AS location_name,
+    CAST(NULL AS VARCHAR(100)) AS location_address_1,
+    CAST(NULL AS VARCHAR(100)) AS location_address_2,
+    CAST(NULL AS VARCHAR(100)) AS location_city,
+    CAST(NULL AS VARCHAR(100)) AS location_state,
+    CAST(NULL AS VARCHAR(100)) AS location_zip
+LIMIT 0
